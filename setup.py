e46f08aedadc601a219256119a0beee7f7d868f2
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2026-02-02 15:10:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirements
+    this function will return a list of requirements
     '''
     requirements = []
     with open(file_path) as file_obj:
